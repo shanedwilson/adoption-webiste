cda358ee5fb236510792cd772e9e5ff471faa563
Shane D Wilson <me@shanedwilson.com>
2018-10-04 18:32:54 -0500

    wip

--- javascripts/components/petComponent.js
+++ javascripts/components/petComponent.js
@@ -5,7 +5,20 @@ const setPets = (newArray) => {
 };
 
 const petsBuilder = (petsArray) => {
-  console.log(pets)
+  let domString = "";
+  petsArray.forEach(pet => {
+    domString +=  `<div class="card" style="width: 18rem;">`
+    domString +=  `  <div class="card-header">${pet.name}</div>`
+    domString +=  `    <img class="card-img-top" src="${pet.imageUrl}" alt="${pet.name}">`
+    domString +=  `    <div class="card-body">`
+    domString +=  `      <h5 class="card-title">${pet.color}</h5>`
+    domString +=  `      <p class="card-text">${pet.specialSkill}</p>`
+    domString +=  `      <div class="card-footer text-muted">${pet.type}</div>`
+    domString +=  `    </div>`
+    domString +=  `  </div>`
+  });
+  printToDom(domString);
+  // createEvents();
 }
 
 export {setPets, petsBuilder};
